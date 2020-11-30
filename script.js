86428411f0bec83762011314e578d6da4f171fc0
Tuyen Nguyen <trongtuyen96@gmail.com>
2020-11-30 23:05:55 +0700

    Add loading category automatically

--- script.js
+++ script.js
@@ -1,117 +1,51 @@
-// const quizData = [{
-//     question: 'How old is Tuyen?',
-//     a: '10',
-//     b: '17',
-//     c: '24',
-//     d: '100',
-//     correct: 'c'
-// },
-// {
-//     question: "What is the most used programming language in 2019?",
-//     a: "Java",
-//     b: "C",
-//     c: "Python",
-//     d: "JavaScript",
-//     correct: "d",
-// },
-// {
-//     question: "Who is the President of US?",
-//     a: "Florin Pop",
-//     b: "Donald Trump",
-//     c: "Ivan Saldano",
-//     d: "Mihai Andrei",
-//     correct: "b",
-// },
-// {
-//     question: "What does HTML stand for?",
-//     a: "Hypertext Markup Language",
-//     b: "Cascading Style Sheet",
-//     c: "Jason Object Notation",
-//     d: "Helicopters Terminals Motorboats Lamborginis",
-//     correct: "a",
-// },
-// {
-//     question: "What year was JavaScript launched?",
-//     a: "1996",
-//     b: "1995",
-//     c: "1994",
-//     d: "none of the above",
-//     correct: "b",
-// }
-// ];
-
-// const quiz = document.getElementById('quiz');
-// const answerEls = document.querySelectorAll('.answer');
-// const questionEl = document.getElementById('question');
-// const a_text = document.getElementById('a_text');
-// const b_text = document.getElementById('b_text');
-// const c_text = document.getElementById('c_text');
-// const d_text = document.getElementById('d_text');
-// const submitBtn = document.getElementById('submit');
-
+const categoryListEl = document.getElementById('categories');
 const categoryEls = document.querySelectorAll('.category');
 const popupEl = document.getElementById('category-popup');
-const popupCloseBtn = document.getElementById('close-popup');
+const popupCloseBtn = document.getElementById("close-popup");
+const popupBackBtn = document.querySelector(".btn-back");
+let curCate = "";
 
-// let currentQuiz = 0;
-// let score = 0;
+const categoryList = ["Animals", "Movies", "Sports"];
 
-// loadQuiz();
+loadCategories();
 
-// function loadQuiz() {
-//     deselectAnswer();
-//     const currentQuizData = quizData[currentQuiz];
+function loadCategories() {
+    categoryList.forEach(item => {
+        const categoryEl = document.createElement('div');
+        categoryEl.classList.add("category");
 
-//     questionEl.innerText = currentQuizData.question;
-//     a_text.innerText = currentQuizData.a;
-//     b_text.innerText = currentQuizData.b;
-//     c_text.innerText = currentQuizData.c;
-//     d_text.innerText = currentQuizData.d;
-// }
+        categoryEl.addEventListener('click', () => {
+            curCate = item.toLowerCase();
+            loadVideoForPopup(curCate);
 
-// function getSelected() {
-//     let answer = undefined;
+            popupEl.children[0].children[1].children[1].innerHTML = category;
+            popupEl.classList.remove('hidden');
+        })
+        categoryEl.innerHTML = `<img src="res/${item}/image.jpg" alt="${item}">
+        <div class="category-body">
+            <h4>${item}</h4>
+        </div>`;
 
-//     answerEls.forEach((answerEl) => {
-//         if (answerEl.checked) {
-//             return answer = answerEl.id;
-//         }
-//     });
+        categoryListEl.appendChild(categoryEl);
+    })
+}
 
-//     return answer;
-// }
+function loadVideoForPopup(category) {
+    const frame = document.getElementById("video-frame");
+    frame.setAttribute('src', "res/" + category + "/video.mp4");
+}
 
-// function deselectAnswer() {
-//     answerEls.forEach((answerEl) => {
-//         answerEl.checked = false;
-//     });
-// }
+categoryEls.forEach((categoryEl) => categoryEl.addEventListener('click', () => {
+    curCate = categoryEl.children[1].children[0].innerHTML.toLowerCase();
+    loadVideoForPopup(curCate);
 
-// submitBtn.addEventListener('click', () => {
-//     // check to see the answer
-//     const answer = getSelected();
-
-//     if (answer) {
-//         if (answer === quizData[currentQuiz].correct) {
-//             score++;
-//         }
-//         currentQuiz++;
-//         if (currentQuiz < quizData.length) {
-//             loadQuiz();
-//         } else {
-//             quiz.innerHTML = `
-//             <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
-
-//             <button onClick="location.reload()">Reload</button>`;
-//         }
-//     }
-// })
-
-categoryEls.forEach((category) => category.addEventListener('click', () => {
+    popupEl.children[0].children[1].children[1].innerHTML = curCate.charAt(0).toUpperCase() + curCate.slice(1);
     popupEl.classList.remove('hidden');
+
 }));
 
-popupCloseBtn.addEventListener('clicked', () => {
+
+popupCloseBtn.addEventListener('click', () => {
     // Stop the video player
     const frame = document.getElementById("video-frame");
     if (frame) {
@@ -120,3 +54,4 @@ popupCloseBtn.addEventListener('clicked', () => {
 
     popupEl.classList.add('hidden');
 });
+
